726d24d7a304be3fed20f0bc6196176d4abbefce
Pritish Nagpal <pritishnagpal@sharechat.co>
2023-02-25 15:30:44 +0530

    fic env

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionURL = process.ENV.MONGO_URL;
+const connectionURL = process.env.MONGO_URL;
 
 mongoose.connect(connectionURL, () => {
   console.log("Database is Connected");
